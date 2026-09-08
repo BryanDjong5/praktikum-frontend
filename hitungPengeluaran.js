e3b988423368cc23f547b22b8cb1e767dfc901b7
BryanDjong5 <bryangerardo392@gmail.com>
2026-09-08 21:35:38 +0700

    perbaikan bikin website expense Tracker

--- hitungPengeluaran.js
+++ hitungPengeluaran.js
@@ -2,30 +2,42 @@ const TombolHitung = document.getElementById("hitungPengeluaran")
 const hasilHitungan = document.getElementById("output");
 const tombolHapusHasil = document.getElementById("hapusHasil");
 
+hasilHitungan.innerHTML = `
+    <h2>Hasil Pengeluaran</h2>
+`;
+
 TombolHitung.addEventListener("click", function() {
     const SemuaNamaPengeluaran = document.querySelectorAll('input[name="keterangan"]');
     const semuaPengeluaran = document.querySelectorAll('input[name="pengeluaran"]');
 
     let total = 0;
-    let cetakan = "";
+    let cetakan = `
+    <h2>Hasil Pengeluaran</h2>`;
 
     for(let p = 0; p < semuaPengeluaran.length; p++){
         const namaPengeluaran = SemuaNamaPengeluaran[p].value;
         const pengeluaran = Number(semuaPengeluaran[p].value);
+
+        if(namaPengeluaran === "" || pengeluaran == ""){
+            continue;
+        }
+        
         total = total + pengeluaran;
         cetakan = cetakan + `
         <p>Nama Pengeluaran: ${namaPengeluaran}</p>
-        <p>Pengeluaran: ${pengeluaran}</p>
+        <p>Pengeluaran: ${pengeluaran.toLocaleString("id-ID")}</p>
         `;
     }
     cetakan = cetakan + `
-    <p>Total pengeluaran: ${total}</p>`;
+    <p>Total pengeluaran: ${total.toLocaleString("id-ID")}</p>`;
 
     hasilHitungan.innerHTML = cetakan;
 });
 
 tombolHapusHasil.addEventListener("click", function(){
-    hasilHitungan.remove();
+    hasilHitungan.innerHTML = `
+    <h2>Hasil Pengeluaran</h2>
+`;
 });
 
 
